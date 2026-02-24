462f922b8a95b3337c3c74676e220ff1e6d74865
Sofiane Laghouati <sofianelaghouatipro@gmail.com>
2026-02-25 00:10:40 +0100

    fix pagination services

--- src/pages/user/services/index.tsx
+++ src/pages/user/services/index.tsx
@@ -90,7 +90,7 @@ export function ServicesPage() {
     isLoading,
     refetch,
     } = useGet<PaginatedResponse<Service>>(
-      withParams("services", { salonId, perPage: 10 }),
+      withParams("services", { salonId, perPage: 100 }),
       { enabled: !!salonId, staleTime: servicesStaleTime },
     );
   const services = servicesResponse?.data ?? [];
